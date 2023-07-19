d747f04b55f3c9d351348fbc72c3f6763343dd2f
Jorge Pallares Catalan <jorge.pallares.catalan@gmail.com>
2023-07-19 23:44:22 +0200

    adding frame for the game

---
+++ pp2.js
@@ -0,0 +1,15 @@
+//make screen for Game
+//make screen for Game Over
+//make screen for Game Win
+//make screen for Game Lose
+//make screen for Game Tie
+//make screen for Game Start
+//make screen for Game Instructions
+//make screen for Game Credits
+//make screen for Game Settings
+
+
+
+
+
+
