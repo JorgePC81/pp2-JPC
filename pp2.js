6f5a6434fc45f4453e7f2c5f716168accf2fd04c
Jorge Pallares Catalan <jorge.pallares.catalan@gmail.com>
2023-07-29 01:54:55 +0200

    adding constants and variables for the game's logic

---
+++ pp2.js
@@ -0,0 +1,9 @@
+/*constants and variables for the game's logic*/
+
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let result;
